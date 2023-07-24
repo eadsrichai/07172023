e54df3ef440848f978991a6640604e1aa52b1e7a
wuttiwong <wutkmutt@hotmail.com>
2023-07-24 14:13:36 +0700

    update

--- db.php
+++ db.php
@@ -6,8 +6,8 @@
 
     $conn = new mysqli($servername,$username,$password,$databasename);
 
-    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+    echo  "Success";
 ?>
