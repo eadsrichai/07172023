6de4e27f3fff873432d557d06b37aef64ee6f4af
wuttiwong <wutkmutt@hotmail.com>
2023-08-07 14:33:09 +0700

    login chk

--- backend/chklogin.php
+++ backend/chklogin.php
@@ -2,8 +2,23 @@
 $u = $_GET['username'];
 $p = $_GET['password'];
 
-echo $u;
-echo  $p;
+include_once('../db.php');
 
+$sql = "SELECT  acc_username, acc_password 
+        FROM  account
+        WHERE  acc_username = '$u'  
+        AND    acc_password = '$p'
+        ";
+
+ $result = $conn->query($sql);
+
+
+ if ($result->num_rows > 0) {
+     while($row = $result->fetch_assoc()) {
+        echo "username and password ถูกต้อง";
+     }
+}else {
+    echo "username and password ไม่ถูกต้อง";
+}
 
 ?>
